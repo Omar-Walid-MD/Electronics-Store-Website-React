491f1b22383b2e7850d0a7018d91d324b5ac29bf
Omar-Walid-MD <omar.madinah49@yahoo.com>
2022-07-25 03:28:12 +0200

    Created website base with navbar and animated front section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import searchIcon from "./img/search.png"
 import './App.css';
+import { useEffect, useState } from 'react';
+import OverviewDisplay from './OverviewDisplay';
 
 function App() {
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="nav-bar">
+          <h1 className="logo">Store Title</h1>
+          <div className="search-bar-container">
+            <img className="search-icon" src={searchIcon} alt="search-icon" />
+            <input className="search-bar" type="search" />
+          </div>
+          <div>
+            <h3>More</h3>
+          </div>
+        </div>
       </header>
+      <div className="page-container">
+        <OverviewDisplay />
+      </div>
     </div>
   );
 }
---
+++ src/OverviewDisplay.js
@@ -0,0 +1,114 @@
+import featureImgList from "./img/features/export.js"
+import { useEffect, useState } from 'react';
+
+function OverviewDisplay()
+{
+    const featureList = [
+        {
+          img: featureImgList[0],
+          desc: "First First First First",
+        },
+        {
+          img: featureImgList[1],
+          desc: "Second Second Second Second",
+        },
+        {
+          img: featureImgList[2],
+          desc: "Third Third Third Third",
+        },
+        {
+          img: featureImgList[3],
+          desc: "Fourth Fourth Fourth Fourth",
+        },
+      ];
+    
+      const [featureindex,setFeatureIndex] = useState(0);
+    
+      function handleFeatureIndex()
+      {
+        if(featureindex===0)
+        {
+          setFeatureIndex(3);
+        }
+        else
+        {
+          setFeatureIndex(x => x - 1);
+        }
+      }
+    
+      function updateFeatureIndex()
+      {
+        setTimeout(() => {
+          handleFeatureIndex();
+        }, 4000);
+      }
+    
+      function GetFeaturePanels()
+      {
+        let resultList = [];
+        for (const key in featureList)
+        {
+          const k = parseInt(key)
+    
+          if(k === featureindex)
+          {
+            
+            if(k===0)
+            {
+              resultList.push(featureList[featureList.length-1],featureList[0],featureList[1]);
+            }
+            else if(k===featureList.length-1)
+            {
+              resultList.push(featureList[featureList.length-2],featureList[featureList.length-1],featureList[0]);
+            }
+            else
+            {
+              resultList.push(featureList[k-1],featureList[k],featureList[k+1]);
+            }
+              
+            break;
+          }    
+        }
+        return resultList
+      }
+      
+     
+      useEffect(()=>{
+    
+        updateFeatureIndex();
+
+        return console.log("re-rendered");
+    
+      },[featureindex]);
+
+      function makeId(length) {
+        let result = "";
+        let chars = "123456789";
+        for (var i = 0; i < length; i++) {
+        result += chars[Math.floor(Math.random() * 9)];
+        }
+        console.log(result)
+        return result;
+      }
+
+    return (
+        <section className="front-container">
+          <h1 className="front-title">Store Title</h1>
+          <div className="feature-panels">
+            {
+              GetFeaturePanels().map((feature,index)=>
+              <div className="feature-panel-container" order={index} key={index}>
+                  <div className="feature-panel" key={makeId(3)}>
+                    <img className="feature-image" src={feature.img}/>
+                    <div className="feature-desc">{feature.desc}</div>
+                  </div>
+              </div>
+              )
+            }
+            
+          </div>
+        </section>
+    )
+}
+
+export default OverviewDisplay;
